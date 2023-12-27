e9f02a3da5b14cd731dbd6bfa175b04fabc0e51d
Ty Huffman <ty@tyhuffman.com>
2023-12-27 12:58:22 -0500

    Added code to support running multiple commands

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
 use std::process::Command;
 
+fn run_command(command: &str) {
+  println!("Running {}", command);
+  Command::new(command)
+      .spawn()
+      .expect("error running command");
+}
+
 fn main() {
-  println!("Running whoami command");
-  Command::new("whoami")
-    .spawn()
-    .expect("Error running command");
+  let commands = vec![
+    "whoami",
+    "ifconfig",
+    "/usr/bin/ip",
+    "/usr/sbin/ip",
+    "/bin/uname",
+    "/bin/cat",
+    "/bin/crontab",
+    "hostname",
+    "iptables",
+    "netstat",
+    "pwd",
+    "route"
+  ];
+
+  for command in commands.iter() {
+    run_command(command);
+  };
 }
